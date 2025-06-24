2ebd34c87484b20b18bd6523b423236ab5440215
Scorubix29 <109562223+Scorubix29@users.noreply.github.com>
2025-06-25 05:35:19 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,6 @@
 //% color="#FF8800" weight=100 icon="\uf11b" block="MakerLine"
 namespace MakerLine {
 
-    function angleToTime(angle: number, speed: number): number {
-        // You can fine-tune this formula for your own robot
-        const baseTime = 1000 // time in ms for 180 degrees at speed 100
-        return (angle / 180) * (baseTime * (100 / speed))
-    }
-
-    //% block="turn right $angle degrees at speed $speed"
-    //% group="Motion"
-    export function turnRightAngle(angle: number, speed: number): void {
-        const time = angleToTime(angle, speed)
-        motionbit.runMotor(leftMotor, MotionBitMotorDirection.Forward, speed)
-        motionbit.runMotor(rightMotor, MotionBitMotorDirection.Backward, speed)
-        basic.pause(time)
-        stopMotors()
-    }
-
-
-
-    //% block="turn left $angle degrees at speed $speed"
-    //% group="Motion"
-    export function turnLeftAngle(angle: number, speed: number): void {
-        const time = angleToTime(angle, speed)
-        motionbit.runMotor(leftMotor, MotionBitMotorDirection.Backward, speed)
-        motionbit.runMotor(rightMotor, MotionBitMotorDirection.Forward, speed)
-        basic.pause(time)
-        stopMotors()
-    }
-
-
     export enum SensorPosition {
         //% block="Left"
         Left,
@@ -43,16 +14,8 @@ namespace MakerLine {
         Right
     }
 
-  
     let pinMap: DigitalPin[] = []
-
-    //% block="set $pos IR sensor pin to $pin"
-    //% group="Setup"
-    export function setSensorPin(pos: SensorPosition, pin: DigitalPin): void {
-        pinMap[pos] = pin
-    }
-
-   
+    let turningFactor = 5.5
 
     let leftMotor: MotionBitMotorChannel = MotionBitMotorChannel.M1
     let rightMotor: MotionBitMotorChannel = MotionBitMotorChannel.M2
@@ -64,6 +27,11 @@ namespace MakerLine {
         rightMotor = right
     }
 
+    //% block="set $pos IR sensor pin to $pin"
+    //% group="Setup"
+    export function setSensorPin(pos: SensorPosition, pin: DigitalPin): void {
+        pinMap[pos] = pin
+    }
 
     //% block="sensor $pos detects black line"
     //% group="Sensors"
@@ -99,14 +67,54 @@ namespace MakerLine {
         motionbit.brakeMotor(rightMotor)
     }
 
+    //% block="turn left $angle degrees at speed $speed"
+    //% group="Motion"
+    export function turnLeftAngle(angle: number, speed: number): void {
+        const time = angleToTime(angle)
+        motionbit.runMotor(leftMotor, MotionBitMotorDirection.Backward, speed)
+        motionbit.runMotor(rightMotor, MotionBitMotorDirection.Forward, speed)
+        basic.pause(time)
+        stopMotors()
+    }
+
+    //% block="turn right $angle degrees at speed $speed"
+    //% group="Motion"
+    export function turnRightAngle(angle: number, speed: number): void {
+        const time = angleToTime(angle)
+        motionbit.runMotor(leftMotor, MotionBitMotorDirection.Forward, speed)
+        motionbit.runMotor(rightMotor, MotionBitMotorDirection.Backward, speed)
+        basic.pause(time)
+        stopMotors()
+    }
+
+    function angleToTime(angle: number): number {
+        return angle * turningFactor
+    }
+
+    //% block="set turning factor to $factor"
+    //% group="Setup"
+    export function setTurningFactor(factor: number): void {
+        turningFactor = factor
+    }
+
     //% block="follow line at speed $speed"
     //% group="Line Following"
     export function followLine(speed: number): void {
-        if (sensorDetectsLine(SensorPosition.Center)) {
-            moveForward(speed)
-        } else if (sensorDetectsLine(SensorPosition.MidLeft)) {
+        let left = sensorDetectsLine(SensorPosition.Left)
+        let midLeft = sensorDetectsLine(SensorPosition.MidLeft)
+        let center = sensorDetectsLine(SensorPosition.Center)
+        let midRight = sensorDetectsLine(SensorPosition.MidRight)
+        let right = sensorDetectsLine(SensorPosition.Right)
+
+        if (left && midLeft) {
+            turnRight(speed)
+        } else if (right && midRight) {
             turnLeft(speed)
-        } else if (sensorDetectsLine(SensorPosition.MidRight)) {
+        } else if (center) {
+            moveForward(speed)
+        } else if (midLeft) {
+            turnLeft(speed)
+        } else if (midRight) {
             turnRight(speed)
         } else {
             stopMotors()
